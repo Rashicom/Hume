acb12d852b1a319fd8c126cf21a7d56129f54b99
Rashicom <rashid.kp484@gmail.com>
2024-09-10 11:59:59 +0530

    template linkes configured as static

--- authorization/urls.py
+++ authorization/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.Test.as_view(), name='test')
+    path('home/', views.Home.as_view(), name='home'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('news/', views.News.as_view(), name='news'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('staff/', views.Staff.as_view(), name='staff'),
+    path('about/', views.About.as_view(), name='about'),
+    path('contact/', views.Contact.as_view(), name='contact'),
+    path('elements/', views.Elements.as_view(), name='elements'),
+    path('gallery/', views.Gallery.as_view(), name='gallery'),
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -4,6 +4,39 @@ from django.views import View
 # Create your views here.
 
 
-class Test(View):
+class Home(View):
     def get(self, request):
         return render(request, 'index.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    
+class News(View):
+    def get(self, request):
+        return render(request, 'news.html')
+    
+class Register(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+class Staff(View):
+    def get(self, request):
+        return render(request, 'staff.html')
+    
+class About(View):
+    def get(self, request):
+        return render(request, 'about.html')
+    
+class Contact(View):
+    def get(self, request):
+        return render(request, 'contact.html')
+    
+class Elements(View):
+    def get(self, request):
+        return render(request, 'elements.html')
+
+class Gallery(View):
+    def get(self, request):
+        return render(request, 'gallery.html')
