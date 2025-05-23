756f018955fb09bf9dcb9f21572865ed0669c992
Rashikitaab <rashid.kp@kitaab.ai>
2025-05-23 14:54:38 +0530

    update(hume):html updated

--- Hume/config/settings.py
+++ Hume/config/settings.py
@@ -27,7 +27,8 @@ environ.Env.read_env(os.path.join(BASE_DIR,"..", '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-
+GDAL_LIBRARY_PATH="/opt/homebrew/Cellar/gdal/3.11.0/lib/libgdal.dylib"
+GEOS_LIBRARY_PATH="/opt/homebrew/Cellar/geos/3.13.1/lib/libgeos_c.dylib"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
